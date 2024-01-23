968b372599ed809e012b7757c9b2513f4d2fb3a4
Aidan <aidang.22@ichat.sp.edu.sg>
2024-01-23 22:10:18 +0800

    added client side validation for login/signup

--- src/login.php
+++ src/login.php
@@ -6,16 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles_login.css">
     <title>Login Form</title>
+
+    <script>
+        function validateForm() {
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+
+            if (email === "" || password === "") {
+                alert("Please fill in all the information.");
+                return false;
+            }
+        }
+    </script>
 </head>
 <body>
 <div class="container">
     <div class="login-container">
         <h1>Login</h1>
-        <form method="post">
+        <form method="post" onsubmit="return validateForm()">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required
                    value="<?php echo $email; ?>"
-
             >
 
             <label for="password">Password:</label>
@@ -23,7 +34,7 @@
                    value="<?php echo $password; ?>"
             >
 
-            <button type="submit" name="submit" >Login</button>
+            <button type="submit" name="submit">Login</button>
         </form>
     </div>
     <p class="signup-text">Don't have an account? <a href="signup.php" class="signup-link">Sign up</a></p>
--- src/signup.php
+++ src/signup.php
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
-<?php include 'signup_validation.php' ;?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles_signup.css">
     <title>Sign Up</title>
+
+    <script>
+        function validateForm() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+
+            if (name === "" || email === "" || password === "") {
+                alert("Please fill in all the information.");
+                return false;
+            }
+        }
+    </script>
 </head>
 <body>
 
 <div class="signup-container">
     <h1>Sign Up</h1>
-    <form method="post">
+    <form method="post" onsubmit="return validateForm()">
         <label for="name">Full Name:</label>
-        <input type="text" id="name" name="name" required
-               value="<?php echo $username; ?>">
+        <input type="text" id="name" name="name" required>
 
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required
-               value="<?php echo $email; ?>">
+        <input type="email" id="email" name="email" required>
 
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required
-               value="<?php echo $password; ?>">
+        <input type="password" id="password" name="password" required>
 
         <button type="submit" name="submit">Sign Up</button>
     </form>
