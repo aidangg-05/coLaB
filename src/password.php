d3d76329775c14d9ca6da4bb73a6d40f33f6bb42
SquidySpez <ianlau.22@ichat.sp.edu.sg>
2024-01-25 15:12:39 +0800

    Added profile page

---
+++ src/password.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_password.css">
+    <title>Password</title>
+</head>
+<body>
+
+<div class="pw-container">
+    <h1>Change Password</h1>
+    <form action="#" method="post">
+        <label for="oldpw">Password:</label>
+        <input type="password" name="password" required>
+
+        <label for="newpw">New Password:</label>
+        <input type="password" name="password" required>
+
+        <label for="cfm-newpw">Confirm New Password:</label>
+        <input type="password" name="password" required>
+
+        <button type="submit">Change Password</button>
+    </form>
+</div>
+
+</body>
+</html>
