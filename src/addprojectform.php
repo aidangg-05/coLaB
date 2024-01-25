096d132bc2044b9fd77746df476ddfc31f7a6ed8
AYunYang <130944403+AYunYang@users.noreply.github.com>
2024-01-25 22:12:22 +0800

    Added a add project page

--- src/addprojectform.php
+++ src/addprojectform.php
@@ -4,33 +4,43 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" type="text/css" href="styles_addprojectform.css">
+    <script type="text/javascript" src="script_addprojectform.js"></script>
+    <!--For icons-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+
 </head>
 <body>
 
 </body>
-    <section class="form">
-        <form>
-            <div class="project_name_input_box">
-                <span>Project Name:</span>
-                <input type="text" class="project_name_input" placeholder="hello">
-            </div>
+    <form>
+        <h1>Add Your Project Here!</h1>
+        <div class="input_box">
+            <span class="input_text">Project Name:</span>
+            <input type="text" class="project_name_input" placeholder="Your project name">
+        </div>
 
-            <div class="enddate_input_box">
-                <span>End Date:</span>
-                <input type="date" class="enddate_input">
-            </div>
+        <div class="input_box">
+            <span class="input_text">End Date:</span>
+            <input type="datetime-local" class="enddate_input">
+        </div>
 
-            <div class="project_desc_input_box">
-                <span>Project Description:</span>
-                <textarea placeholder="input your project description here"></textarea>
-            </div>
+        <div class="input_box">
+            <span class="input_text">Project Description:</span>
+            <textarea placeholder="Input your project description here" class="desc_input"></textarea>
+        </div>
 
-            <div class="add_users_box">
-                <span>Add Other Users:</span>
-                <input type="button" value="Add Users">
-            </div>
+        <div class="input_box">
+            <span class="input_text">Add Other Users:</span>
+            <button class="add_user_button">
+                <span class="material-symbols-outlined">group_add</span>
+            </button>
+        </div>
 
-        </form>
-
-    </section>
+        <div class="form_buttons">
+            <input type="button" value="Back" class="back" onclick="backtomain()">
+            <input type="reset" class="reset">
+            <input type="button" value="Submit" class="submit">
+        </div>
+    </form>
 </html>
