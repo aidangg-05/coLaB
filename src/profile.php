73215617fbaf7a83063ecf33ae9a356479f3b080
SquidySpez <130943602+SquidySpez@users.noreply.github.com>
2024-01-26 12:52:30 +0800

    added back button to profile page

--- src/profile.php
+++ src/profile.php
@@ -5,42 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles_profile.css">
     <title>Profile</title>
-    <script>
-        function goback(){
-            window.location.href = "index.php";
-        }
-    </script>
 </head>
 <body>
-<div class="profile-container">
-    <div class="top">
-        <img src="noimg.jpg" alt="Profile Image">
-        <text class="name">Insert Name</text>
-        <button type="button" id="logout">Log Out</button>
-    </div>
-    <p>
-        <span class="textA">Account</span>
-        <br />
-        <span class="textB">Update your personal information</span>
-    </p>
-    <form action="#" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" placeholder="Insert Existing Username Value">
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Insert Existing Email Value">
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Enter password to confirm" required>
-
-        <a class="chg-password" href="password.php">Change Password</a>
-
-        <div class="buttons">
-            <button type="reset" id="reset">Discard</button>
-            <button type="submit" id="submit">Update Info</button>
+    <div class="profile-container">
+        <div class="top">
+            <img src="noimg.jpg" alt="Profile Image">
+            <text class="name">Insert Name</text>
+            <button type="button" id="logout">Log Out</button>
         </div>
-    </form>
-</div>
+        <p>
+            <span class="textA">Account</span>
+            <br />
+            <span class="textB">Update your personal information</span>
+        </p>
+        <form action="#" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" placeholder="Insert Existing Username Value">
 
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Insert Existing Email Value">
+
+            <a class="chg-password" href="password.html">Change Password</a>
+
+            <div class="buttons">
+                <button type="button" id="back" onclick="window.location.href='https://www.youtube.com/watch?v=TTG8o4gnqVw'">Back</button>
+                <button type="reset" id="reset">Discard</button>
+                <button type="submit" id="submit">Update Info</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
