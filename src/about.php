869a2b8ad2e1d15cb3131cfb8e615516dc0ed89f
SquidySpez <130943602+SquidySpez@users.noreply.github.com>
2024-01-27 03:53:17 +0800

    added about page

---
+++ src/about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_about.css">
+    <title>About</title>
+</head>
+<body>
+    <div class="about-container">
+        <p>
+            <span id="title-top">THE TEAM</span><br />
+            <span id="title-bottom">&amp; THEIR ROLES</span>
+        </p>
+        <div class="top">
+            <p class="left">
+                <span class="name">Insert Name</span><br />
+                <span class="role">Insert Role</span><br /><br />
+                <span class="desc">Talk about what you did ig blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</span>
+            </p>
+            <p class="right">
+                <span class="name">Insert Name</span><br />
+                <span class="role">Insert Role</span><br /><br />
+                <span class="desc">Talk about what you did ig blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</span>
+            </p>
+        </div>
+        <div class="bottom">
+            <p class="left">
+                <span class="name">Insert Name</span><br />
+                <span class="role">Insert Role</span><br /><br />
+                <span class="desc">Talk about what you did ig blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</span>
+            </p>
+            <p class="right">
+                <span class="name">Insert Name</span><br />
+                <span class="role">Insert Role</span><br /><br />
+                <span class="desc">Talk about what you did ig blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</span>
+            </p>
+        </div>
+        <div class="buttons">
+            <button type="button" id="back" onclick="window.location.href='https://www.youtube.com/watch?v=TTG8o4gnqVw'">Back</button>
+        </div>
+    </div>
+</body>
+</html>
