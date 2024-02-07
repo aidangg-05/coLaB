ad40021bf4ef73cbb67352fbd6ef68d1814fb2ff
SquidySpez <130943602+SquidySpez@users.noreply.github.com>
2024-02-07 23:21:35 +0800

    added confirm page

---
+++ src/confirm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_confirm.css">
+    <title>Confirm</title>
+</head>
+<body>
+    <div class="confirm-container">
+        <img src="confirm.png" alt="Checkmark">
+        <h1>Transaction Successful</h1>
+        <h3>Thank you for supporting our site!</h3>
+        <div class="buttons">
+            <button type="button" id="back" onclick="window.location.href='index.php'">Back</button>
+        </div>
+    </div>
+</body>
+</html>
