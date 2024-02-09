cdff66db1eaeb28f30d5bd3f8d5d4cc89ee0e5fa
Aidan <aidang.22@ichat.sp.edu.sg>
2024-02-09 21:00:37 +0800

    urm

--- src/helpme.php
+++ src/helpme.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Document</title>
     <link rel="stylesheet" href="helpme.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
@@ -20,7 +18,6 @@
             <li><a href="locations.html">Locations</a></li>
             <li><p class="dot">•</p></li>
             <li><a href="index.html">Home</a></li>
-
         </div>
         <li>
             <button id="button"><img class="hamburger" src="images/burger.png"/></button>
@@ -32,7 +29,6 @@
         </li>
     </ul>
 </div>
-<script src="helpme.js"></script>
 
 <div class="overlay" id="overlay"></div>
 <div class="popup" id="notificationPopup">
@@ -50,5 +46,6 @@
         </ul>
     </div>
 </div>
+<script src="helpme.js"></script>
 </body>
 </html>
